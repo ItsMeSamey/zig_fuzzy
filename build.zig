16f150bd262d09a0c0c41720b60c7cd82b25bf49
ItsMeSamey <sameychain5041@gmail.com>
2025-06-14 23:08:18 +0530

    updated build.zig

--- build.zig
+++ build.zig
@@ -6,15 +6,18 @@ pub fn build(b: *std.Build) void {
 
   const fuzzy_module = b.addModule("fuzzy", .{
     .root_source_file = b.path("src/root.zig"),
+    .single_threaded = true,
   });
 
   const options = b.addOptions();
   fuzzy_module.addOptions("build", options);
 
   const lib_unit_tests = b.addTest(.{
-    .root_source_file = b.path("src/root.zig"),
-    .target = target,
-    .optimize = optimize,
+    .root_module = b.addModule(.{
+      .root_source_file = b.path("src/root.zig"),
+      .target = target,
+      .optimize = optimize,
+    }),
   });
 
   const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
